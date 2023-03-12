404a0a15c18fce2bd8f913731529fe79b1fbb351
kubaneko <kubanek0ondrej@gmail.com>
2023-03-12 22:46:45 +0100

    fixed up types added mkAp

--- leval.cpp
+++ leval.cpp
@@ -4,21 +4,20 @@
 #include <memory>
 #include <map>
 #include <stack>
-#include <memory>
-#include <vector>
+#include <cassert>
 
-enum class GmValEnum {
+enum GmValEnum {
     GLOB, 
     VALUE, 
     ARG, 
     LOCAL
 };
 
-using GmVal=std::pair<GmValEnum,std::variant<int,std::string>>;
+using GmVal=std::pair<GmValEnum,std::variant<std::string,int,int,int>>;
 
-enum class GmInstrEnum { // Instructions for the Stack machine
-    PUSH, 
+enum GmInstrEnum { // Instructions for the Stack machine
     SLIDE, 
+    PUSH, 
     COND,
     MKAP, 
     EVAL, 
@@ -30,13 +29,18 @@ enum class GmInstrEnum { // Instructions for the Stack machine
 };
 
 struct GmInstr{
+    using GmInstrData=std::variant<int, GmVal, 
+                std::pair<std::vector<GmInstr>,std::vector<GmInstr>>,std::monostate>;
     GmInstrEnum instr;
-    std::variant<std::monostate,int, GmVal, 
-                std::pair<std::vector<GmInstr>,std::vector<GmInstr>>> data;
+    GmInstrData data;
+    GmInstr(GmInstrEnum instr, GmInstrData&& data){
+        instr=instr;
+        data=std::move(data);
+    }
 };
 
-using GmInstrData=<std::monostate,int, GmVal, 
-                std::pair<std::vector<GmInstr>,std::vector<GmInstr>>>;
+using GmInstrData=std::variant<int, GmVal, 
+            std::pair<std::vector<GmInstr>,std::vector<GmInstr>>,std::monostate>;
 
 struct Arg{
     int arg;
@@ -61,116 +65,154 @@ int getArg(int numArg){
 };
 };
 
-using GmNode=std::variant<int,SCo,App>>;
-
-// vector stack with exposed underlying vector
-// because we need indexing, insert of vectors and other operations
-class i_stack : public std::stack<std::shared_pointer<GmNode>,
-                        std::vector<std::shared_pointer<GmNode>>>{
-    using std::stack<std::shared_pointer<GmNode>>::c;
-}
-
-
-static std::map<std::string, LEfunction> function_library;
-
-class G_machine{
-    std::map<std::string, SCo> used_function_library;
-    // stack but we need indexing
-    using curr_stack_t = std::vector<>;
-    curr_stack_t curr_stack;
-    std::stack<std::pair<std::stack<GmInstr>,curr_stack_t>> dump;
-    std::stack<GmInstr> instructs;
-    int eval(){
-        while (!instucts.empty()){
-            GmInstr* next=instructs.top();
-            instructs.pop();
-            switch (next->intr) {
-                case PUSH
-                    GmVal* save=curr_stack.top();
-                    switch (save->first) {
-                        case GmValEnum.GLOB
-                            std::string name=std::get<GLOB>(save->second);
-                            
-                        break;
-                        case GmValEnum.VALUE, 
-                        break;
-                        case GmValEnum.ARG, 
-                        break;
-                        case GmValEnum.LOCAL
-                        break;
-                    }
-                break;
-                case SLIDE
-                    GmVal* save=curr_stack.top();
-                    int num=std::get<SLIDE>(next->data);
-                    for (int i=0;i<=num;++i){
-                        curr_stack.pop();
-                    }
-                    curr_stack.push(save);
-                break;
-                case COND
-                    GmVal* cond=curr_stack.top();
-                    auto code_pair=std::get<COND>(next->data);
-                    curr_stack.pop();
-                    assert(cond==GmValEnum.Num);
-                    if (cond.second!=0){
-                        for(int i=code_pair.first.length()-1; i>-1;--i){
-                            curr_stack.push(code_pair.first[i]);
-                        }
-                    } else {
-                        for(int i=code_pair.second.length()-1; i>-1;--i){
-                            curr_stack.push(code_pair.second[i]);
-                        }
-                    }
-                    // Add the new code to instructs
-                break;
-                case MKAP 
-                break;
-                case UNWIND
-                break;
-                case EVAL 
-                    GmVal* nd=curr_stack.top();
-                    curr_stack.pop();
-                    dump.push(pair(instucts,curr_stack));
-                    instructs=std::pair(GmInstrEnum.UNWIND,std::monostate);
-                    curr_stack=std::stack(nd);
-                break;
-                case ADD 
-                    auto arg=getNums();
-                    curr_stack.push(arg.second+arg.first);
-                break;
-                case SUB 
-                    auto arg=getNums();
-                    curr_stack.push(arg.second-arg.first);
-                break;
-                case DIV 
-                    auto arg=getNums();
-                    curr_stack.push(arg.second/arg.first);
-                break;
-                case MUL 
-                    auto arg=getNums();
-                    curr_stack.push(arg.second*arg.first);
-                break;
-            }
-        }
-        return 1       
-    }
-    std::pair<int,int> getNums (){
-        GmVal* a = curr_stack.top();
-        curr_stack.pop();
-        GmVal* b = curr_stack.top();
-        curr_stack.pop();
-        assert(a.first==GmValEnum.Num && b.first==GmValEnum.Num);
-        return pair(std::get<1>(a.second),std::get<1>(b.second));
-    }
-};
-
 struct SCo{
     int arity;
     std::vector<GmInstr> code;
-}
+};
+
+struct App;
+
+using GmNode=std::variant<int,SCo*,App>;
 
 struct App{
-    void* function
-    void* arg
-}
+    std::shared_ptr<GmNode> function;
+    std::shared_ptr<GmNode> arg;
+
+    App(std::shared_ptr<GmNode>&& func, std::shared_ptr<GmNode>&& arg){
+        function=std::move(func);
+        arg=std::move(arg);
+    }
+};
+
+enum GmNodeEnum{
+    NUME,
+    SCOE,
+    APPE,
+};
+
+// vector stack with exposed underlying vector
+// because we need indexing, insert of vectors and other operations
+template<typename T>
+class i_stack : public std::stack<T,std::vector<T>>{
+    protected: 
+        using std::stack<T,std::vector<T>>::c;
+    public:
+    // reverse indexing
+    T& operator[]( size_t pos ){
+        assert(c.size()>0);
+        return c[c.size()-1-pos];
+    }
+
+    void insert(std::vector<T>& to_insert){
+        c.insert(c.end(),to_insert.begin(),to_insert.end());
+    }
+    void insert(i_stack<T>&& to_insert){
+        c.insert(c.end(),to_insert.c.begin(),to_insert.c.end());
+    }
+};
+using stack_i=i_stack<std::shared_ptr<GmNode>>;
+
+
+std::map<std::string, std::shared_ptr<SCo>> function_library;
+
+class G_machine{
+    std::map<std::string, std::shared_ptr<SCo>> used_function_library;
+    // stack but we need indexing
+    stack_i curr_stack;
+    std::stack<std::pair<i_stack<GmInstr>,stack_i>> dump;
+    i_stack<GmInstr> code;
+    void perform_instr(GmInstr& next){
+        switch (next.instr) {
+            case PUSH: {
+                push(std::get<PUSH>(next.data));
+            } break;
+            case SLIDE: {
+                auto saves=std::move(curr_stack.top());
+                int num=std::get<SLIDE>(next.data);
+                for (int i=0;i<=num;++i){
+                    curr_stack.pop();
+                }
+                curr_stack.push(saves);
+            } break;
+            case COND: {
+                auto cond=std::move(curr_stack.top());
+                auto code_pair=std::get<COND>(next.data);
+                curr_stack.pop();
+                assert(cond->index()==NUME);
+                if (std::get<NUME>(*cond)==0){
+                    code.insert(code_pair.first);
+                } else {
+                    code.insert(code_pair.second);
+                }
+                // Add the new code to instructs
+            } break;
+            case MKAP: {
+                auto x=std::move(curr_stack.top());
+                curr_stack.pop();
+                auto f=std::move(curr_stack.top());
+                curr_stack.pop();
+                curr_stack.push(std::make_shared<GmNode>(App(std::move(f),std::move(x))));
+            } break;
+            case UNWIND: {
+                unwind();
+            } break;
+            case EVAL: {
+                auto nd=std::move(curr_stack.top());
+                curr_stack.pop();
+                dump.push(std::pair(code,curr_stack));
+                code=i_stack<GmInstr>();
+                code.push(GmInstr(UNWIND,std::monostate()));
+                curr_stack=stack_i();
+                curr_stack.push(std::move(nd));
+            } break;
+            case ADD: {
+                auto arg=getNums();
+                curr_stack.push(std::move(std::make_shared<GmNode>(arg.second+arg.first)));
+            } break;
+            case SUB: {
+                auto arg=getNums();
+                curr_stack.push(std::move(std::make_shared<GmNode>(arg.second-arg.first)));
+            } break;
+            case DIV: {
+                auto arg=getNums();
+                curr_stack.push(std::move(std::make_shared<GmNode>(arg.second/arg.first)));
+            } break;
+            case MUL: {
+                auto arg=getNums();
+                curr_stack.push(std::move(std::make_shared<GmNode>(arg.second*arg.first)));
+            } break;
+        }
+    }
+    int eval(){
+        while (!code.empty()){
+            perform_instr(code.top());
+            code.pop();
+        }
+        return 1;       
+    }
+    void unwind(){
+    }
+    void push(GmVal val){
+        switch (val.first) {
+            case GLOB: {
+                SCo* sco=used_function_library[std::get<GLOB>(val.second)].get();
+                curr_stack.push(std::make_shared<GmNode>(sco));
+            } break;
+            case VALUE: {
+            } break; 
+            case ARG: {
+            } break; 
+            case LOCAL: {
+            } break; 
+        }
+    }
+    std::pair<int,int> getNums (){
+        auto&& a = std::move(curr_stack.top());
+        curr_stack.pop();
+        auto&& b = std::move(curr_stack.top());
+        curr_stack.pop();
+        assert(a->index()==NUME && b->index()==NUME);
+        return std::pair(std::get<NUME>(*a),std::get<NUME>(*b));
+    }
+};
+
